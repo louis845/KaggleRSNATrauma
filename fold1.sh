713538227c59ed6437370713761fdeb38982df84
louis <louis321yh@gmail.com>
2023-08-31 22:25:14 +0800

    Updated training script.

--- fold1.sh
+++ fold1.sh
@@ -8,4 +8,4 @@ while true; do
     sleep 10
 done
 
-python training_ROI_preds.py --learning_rate 1e-3 --model ROI_fold1 --device 0 --epochs 30 --train_data segmentation_fold1_train --val_data segmentation_fold1_val
+python training_ROI_preds.py --learning_rate 1e-3 --model ROI_fold1 --device 0 --epochs 30 --train_data segmentation_fold0_train --val_data segmentation_fold0_val
