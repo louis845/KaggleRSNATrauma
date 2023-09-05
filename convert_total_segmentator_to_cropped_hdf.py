09259ab08ffce8f1420a97a153427727e735dafe
louis <louis321yh@gmail.com>
2023-09-05 21:53:50 +0800

    Added auto start script for converting total segmentator predictions to resized and cropped segmentations.

--- convert_total_segmentator_to_cropped_hdf.py
+++ convert_total_segmentator_to_cropped_hdf.py
@@ -17,7 +17,7 @@ ct_folder = os.path.join("data", "train_images")
 seg_folder = "total_segmentator_results"
 cropped_segmentation_folder = "total_segmentator_hdf_cropped"
 
-if __name__ == "__main__":
+def main_func():
     patient_id_table = pd.read_csv("data/train_series_meta.csv", index_col=1)
 
     if not os.path.isdir(cropped_segmentation_folder):
@@ -28,7 +28,7 @@ if __name__ == "__main__":
         file_path = os.path.join(seg_folder, series_file)
 
         output_file = os.path.join(cropped_segmentation_folder, series_id + ".hdf5")
-        if os.path.isfile(output_file): # if already processed, skip
+        if os.path.isfile(output_file):  # if already processed, skip
             continue
         # laod segmentation
         with h5py.File(file_path, "r") as f:
@@ -36,12 +36,18 @@ if __name__ == "__main__":
 
         patient_id = patient_id_table.loc[int(series_id), "patient_id"]
         series_folder = os.path.join("data", "train_images", str(patient_id), series_id)
-        ct_3D_image, z_positions, dcm_data = convert_to_npy.get_array_and_zpos(series_folder)  # convert dicom series to numpy array
+        ct_3D_image, z_positions, dcm_data = convert_to_npy.get_array_and_zpos(
+            series_folder)  # convert dicom series to numpy array
 
         segmentation_labels = segmentation_processing.translate_labels(segmentation_labels)
         segmentation_arr = segmentation_processing.to_class_array(dcm_data, segmentation_labels)
 
-        _, segmentation_arr = convert_to_hdf_cropped.crop_3d_volume(ct_3D_image, segmentation_arr)  # crop the 3D volume to the smallest possible size
+        _, segmentation_arr = convert_to_hdf_cropped.crop_3d_volume(ct_3D_image,
+                                                                    segmentation_arr)  # crop the 3D volume to the smallest possible size
 
         with h5py.File(os.path.join(cropped_segmentation_folder, "{}.hdf5".format(series_id)), "w") as f:
-            f.create_dataset("segmentation_arr", data=segmentation_arr, dtype=np.uint8, compression="gzip", compression_opts=0)
+            f.create_dataset("segmentation_arr", data=segmentation_arr, dtype=np.uint8, compression="gzip",
+                             compression_opts=0)
+
+if __name__ == "__main__":
+    main_func()
---
+++ convert_total_segmentator_to_cropped_hdf_delayed.py
@@ -0,0 +1,32 @@
+import time
+import os
+
+import convert_total_segmentator_to_cropped_hdf
+import pandas as pd
+
+if __name__ == "__main__":
+    patient_injuries = pd.read_csv("data/train.csv", index_col=0)
+    patients_with_injuries = list(patient_injuries.loc[patient_injuries["any_injury"] == 1].index)
+
+    series_with_expert_segmentations = [int(x[:-5]) for x in os.listdir("data/segmentations")]
+    count = 0
+    for patient_id in patients_with_injuries:
+        patient_folder = os.path.join("data", "train_images", str(patient_id))
+        for series_id in os.listdir(patient_folder):
+            if int(series_id) not in series_with_expert_segmentations:
+                count += 1
+
+    print("CT segmentations required from Total Segmentator:   {}".format(count))
+
+    while True:
+        completed = len(os.listdir("total_segmentator_results"))
+        if completed == count:
+            print("Total segmentator process completed. Cropping and converting...")
+            break
+        else:
+            print("Total segmentator processes still running. Completed {} out of {}".format(completed, count))
+        time.sleep(600)
+
+    time.sleep(100)
+    
+    convert_total_segmentator_to_cropped_hdf.main_func()
