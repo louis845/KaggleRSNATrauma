52b663f1dc6f16ab40838806941f627bc1d2d9cf
Louis Chau <louis321yh@gmail.com>
2023-09-07 00:01:35 +0800

    Decreased number of blocks.

--- fold2.sh
+++ fold2.sh
@@ -8,4 +8,4 @@ while true; do
     sleep 10
 done
 
-python training_ROI_preds.py --learning_rate 3e-4 --momentum 0.99 --num_extra_nonexpert_training 80 --model ROI_fold2_3d_extra_biased --device 0 --epochs 120 --num_extra_steps 1 --train_data segmentation_fold1_train --val_data segmentation_fold1_val --num_slices 9 --hidden_blocks 1 2 6 8 23 4 --use_3d_prediction --use_async_sampler --positive_weight 5.0
+python training_ROI_preds.py --learning_rate 3e-4 --momentum 0.99 --num_extra_nonexpert_training 80 --model ROI_fold2_3d_extra_biased --device 0 --memory_limit 0.495 --epochs 120 --num_extra_steps 1 --train_data segmentation_fold1_train --val_data segmentation_fold1_val --num_slices 9 --hidden_blocks 1 2 6 8 20 4 --use_3d_prediction --use_async_sampler --positive_weight 5.0
--- fold3.sh
+++ fold3.sh
@@ -8,4 +8,4 @@ while true; do
     sleep 10
 done
 
-python training_ROI_preds.py --learning_rate 3e-4 --momentum 0.99 --num_extra_nonexpert_training 80 --model ROI_fold3_3d_extra_biased --device 1 --epochs 120 --num_extra_steps 1 --train_data segmentation_fold2_train --val_data segmentation_fold2_val --num_slices 9 --hidden_blocks 1 2 6 8 23 4 --use_3d_prediction --use_async_sampler --positive_weight 5.0
+python training_ROI_preds.py --learning_rate 3e-4 --momentum 0.99 --num_extra_nonexpert_training 80 --model ROI_fold3_3d_extra_biased --device 1 --epochs 120 --num_extra_steps 1 --train_data segmentation_fold2_train --val_data segmentation_fold2_val --num_slices 9 --hidden_blocks 1 2 6 8 20 4 --use_3d_prediction --use_async_sampler --positive_weight 5.0
