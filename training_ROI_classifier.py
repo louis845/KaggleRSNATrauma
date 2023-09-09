2a921b3c2ce6ffcf0414e4ea3f4a452548aa9ff2
Louis Chau <louis321yh@gmail.com>
2023-09-09 19:59:08 +0800

    Fixed a bug.

--- training_ROI_classifier.py
+++ training_ROI_classifier.py
@@ -260,9 +260,9 @@ def single_training_step_injury(model_: torch.nn.Module, optimizer_: torch.optim
             deep_class_losses.append(class_loss.item())
 
         if is_ternary:
-            class_loss = torch.nn.functional.nll_loss(torch.log(torch.clamp(pred_probas[k], min=1e-10)), torch.max(injury_labels[k], dim=0, keepdim=True), reduction="mean")
+            class_loss = torch.nn.functional.nll_loss(torch.log(torch.clamp(pred_probas[k], min=1e-10)), torch.max(injury_labels[k], dim=0, keepdim=True)[0], reduction="mean")
         else:
-            class_loss = torch.nn.functional.binary_cross_entropy(pred_probas[k], torch.max(injury_labels[k], dim=0, keepdim=True).unsqueeze(0), reduction="mean")
+            class_loss = torch.nn.functional.binary_cross_entropy(pred_probas[k], torch.max(injury_labels[k], dim=0, keepdim=True)[0].unsqueeze(0), reduction="mean")
         loss += class_loss
         class_losses.append(class_loss.item())
 
@@ -388,7 +388,7 @@ def training_step(record: bool):
                         organ_slice_injury_key = MetricKeys.get_injury_metric_key_by_class_code(class_code, MetricKeys.METRIC_TYPE_SLICE_INJURY)
 
                         train_metrics[organ_loss_key].add(class_losses[class_code].item(), 1)
-                        train_metrics[organ_injury_key].add(pred_classes[class_code], torch.max(per_slice_class_labels[class_code], dim=0, keepdim=True))
+                        train_metrics[organ_injury_key].add(pred_classes[class_code], torch.max(per_slice_class_labels[class_code], dim=0, keepdim=True)[0])
                         train_metrics[organ_slice_injury_key].add(per_slice_pred_classes[class_code], per_slice_class_labels[class_code])
                     train_metrics[MetricKeys.INJURY_LOSS].add(loss, 1)
 
@@ -557,7 +557,7 @@ def validation_step():
                                                                                             MetricKeys.METRIC_TYPE_SLICE_INJURY)
 
                     val_metrics[organ_loss_key].add(class_losses[class_code].item(), 1)
-                    val_metrics[organ_injury_key].add(pred_classes[class_code], torch.max(per_slice_class_labels[class_code], dim=0, keepdim=True))
+                    val_metrics[organ_injury_key].add(pred_classes[class_code], torch.max(per_slice_class_labels[class_code], dim=0, keepdim=True)[0])
                     val_metrics[organ_slice_injury_key].add(per_slice_pred_classes[class_code], per_slice_class_labels[class_code])
                 val_metrics[MetricKeys.INJURY_LOSS].add(loss, 1)
 
