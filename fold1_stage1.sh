42bf256c32b8438b579cacbf90ec9289770b568a
Louis Chau <louis321yh@gmail.com>
2023-09-12 21:40:42 +0800

    Added training of ROI predictions to localize organs.

---
+++ fold1_stage1.sh
@@ -0,0 +1,11 @@
+INTEGRITY_FILE="FILES_COMPLETED.txt"
+
+while true; do
+    if [ -f "$INTEGRITY_FILE" ]; then
+        echo "File $INTEGRITY_FILE exists. Running remaining scripts."
+        break
+    fi
+    sleep 10
+done
+
+python training_ROI_preds.py --learning_rate 3e-4 --momentum 0.99 --num_extra_nonexpert_training 80 --model ROI_stage1_fold1 --device 0 --memory_limit 0.495 --epochs 20 --num_extra_steps 19 --train_data ROI_classifier_fold0_train --val_data ROI_classifier_fold0_val --num_slices 9 --hidden_blocks 1 2 6 8 19 4 --use_async_sampler
