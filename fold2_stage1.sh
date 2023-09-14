c068d1cb38427d8687d358a78c938f357050f34a
louis <louis321yh@gmail.com>
2023-09-14 21:16:38 +0800

    Updated training script

--- fold2_stage1.sh
+++ fold2_stage1.sh
@@ -8,4 +8,4 @@ while true; do
     sleep 10
 done
 
-python training_ROI_preds.py --learning_rate 3e-4 --momentum 0.99 --num_extra_nonexpert_training 80 --model ROI_stage1_fold2 --device 0 --memory_limit 0.495 --epochs 36 --num_extra_steps 9 --train_data ROI_classifier_fold1_train --val_data ROI_classifier_fold1_val --num_slices 9 --hidden_blocks 1 2 6 8 19 4 --use_async_sampler
+python training_ROI_preds.py --learning_rate 3e-4 --momentum 0.99 --num_extra_nonexpert_training 80 --model ROI_stage1_fold2 --device 0 --memory_limit 0.495 --epochs 36 --num_extra_steps 14 --train_data ROI_classifier_fold1_train --val_data ROI_classifier_fold1_val --num_slices 9 --hidden_blocks 1 2 6 8 19 4 --use_async_sampler
